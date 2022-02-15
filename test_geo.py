dc620af22bef66a1c32605ebcf2718c9177ede9e
Ben Domb <bd439@cam.ac.uk>
2022-02-15 21:33:37 +0000

    Including Test 1B

--- test_geo.py
+++ test_geo.py
@@ -1,4 +1,5 @@
-# from floodsystem.geo import stations_by_distance, stations_within_radius, rivers_with_station, stations_by_river, rivers_by_station_number
+from floodsystem.geo import stations_by_distance 
+# stations_within_radius, rivers_with_station, stations_by_river, rivers_by_station_number
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import MonitoringStation
 
@@ -39,19 +40,19 @@ town3 = "Town 3"
 TestStation3 = MonitoringStation(station_id3, measure_id3, label3, coord3, typical_range3, river3, town3)
 
 # list of TestStations
-# test_stations = [TestStation1, TestStation2, TestStation3]
+test_stations = [TestStation1, TestStation2, TestStation3]
 
-# '''----------------Task 1B Test-----------------'''
-# # test for stations_by_distance()
-# def test_stations_by_distance():
+'''----------------Task 1B Test-----------------'''
+# test for stations_by_distance()
+def test_stations_by_distance():
 
-#     # applying function to list
-#     TestResult1B = stations_by_distance(test_stations, (0, 0))
+    # applying function to list
+    TestResult1B = stations_by_distance(test_stations, (0, 0))
 
-#     # check: results should be in this order as TestStation 2 is the closest to (0,0), followed by TestStation 3 and then TestStation 1 
-#     assert (TestResult1B[0][0], TestResult1B[1][0], TestResult1B[2][0]) == (TestStation2, TestStation3, TestStation1)
+    # check: results should be in this order as TestStation 2 is the closest to (0,0), followed by TestStation 3 and then TestStation 1 
+    assert (TestResult1B[0][0], TestResult1B[1][0], TestResult1B[2][0]) == (TestStation2, TestStation3, TestStation1)
 
-# test_stations_by_distance()
+test_stations_by_distance()
 
 # '''----------------Task 1C Test-----------------'''
 
